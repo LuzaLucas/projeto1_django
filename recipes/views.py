dacd6f694ef90501d3a292aa35629ee2ddb3220b
Lucas Luza <luzalucas1999@gmail.com>
2024-03-06 16:38:55 -0300

    criação do app recipes

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('HOME 1')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE 1')
+
+
+def contato(request):
+    return HttpResponse('CONTATO 1')
+
+
