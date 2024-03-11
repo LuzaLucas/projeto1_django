1b9d5c96c9879c6868dfb4171c82440ad0742277
Lucas Luza <luzalucas1999@gmail.com>
2024-03-11 16:10:54 -0300

    corrigindo os cards e adicionando nomes únicos para as URLs

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from recipes import views
 
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home), # Home
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"), # Home
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(
         request,
         'recipes/pages/home.html',
-        {'name': 'Lucas',}
+        {'recipes': [make_recipe() for _ in range(10)],}
     )
 
 
@@ -14,6 +15,6 @@ def recipe(request, id):
     return render(
         request,
         'recipes/pages/recipe-view.html',
-        {'name': 'Lucas',}
+        {'recipe': make_recipe(), 'is_detail_page': True,}
     )
 
