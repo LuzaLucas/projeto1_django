9011d0a0e51a29e3401b7f6d8275fa25f562acd9
Lucas Luza <luzalucas1999@gmail.com>
2024-04-01 16:31:11 -0300

    Using clean_field to validate specific fields

--- authors/forms.py
+++ authors/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 def add_attr(field, attr_name, attr_new_val):
     existing_attr = field.widget.attrs.get(attr_name, '')
     field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+    
+    
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
 
 
 class RegisterForm(forms.ModelForm):
@@ -13,6 +18,7 @@ class RegisterForm(forms.ModelForm):
         add_attr(self.fields['username'],'placeholder', 'Your Username goes here :)')
         add_attr(self.fields['email'],'placeholder', 'Your Email goes here :)')
         add_attr(self.fields['last_name'],'placeholder', 'Your Last name goes here :)')
+        add_attr(self.fields['username'], 'css', 'a-css-class')
     
     password = forms.CharField(
         required=True,
@@ -68,3 +74,29 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password here',
             })
         }
+        
+        
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+        
+        if 'atenção' in data: #type: ignore
+            raise ValidationError(
+                'Não digite %(value)s no campo password',
+                code='invalid',
+                params={ 'value': '"atenção"' }
+            )
+        
+        return data
+    
+    
+    def clean_first_name(self): 
+        data = self.cleaned_data.get('first_name')
+        
+        if 'John Doe' in data: #type: ignore
+            raise ValidationError(
+                'Não digite %(value)s no campo first name',
+                code='invalid',
+                params={ 'value': '"John Doe"' }
+            )
+        
+        return data
