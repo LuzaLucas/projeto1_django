0ecb4ed921112693c048877ddc07d85df2bb2978
Lucas Luza <luzalucas1999@gmail.com>
2024-04-04 18:27:38 -0300

    grouping functional tests with pytest markers and using mixins to reuse class methods

--- tests/functional_tests/recipes/base.py
+++ tests/functional_tests/recipes/base.py
@@ -1,9 +1,10 @@
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from utils.browser import make_chrome_browser
+from recipes.tests.test_recipe_base import RecipeMixin
 
 
-class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase, RecipeMixin):
     def setUp(self) -> None:
         self.browser = make_chrome_browser()
         return super().setUp()
@@ -14,5 +15,5 @@ class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
         return super().tearDown()
     
     
-    def sleep(self, seconds=5):
+    def sleep(self, seconds=1):
         time.sleep(seconds)
