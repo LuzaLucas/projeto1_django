2914dda7eb1e248ab36bdc6c3071f0addfc3d74b
Lucas Luza <luzalucas1999@gmail.com>
2024-04-08 14:55:08 -0300

    testing invalid user and password; fully testing logout

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -67,4 +67,29 @@ class AuthorsLoginText(AuthorsBaseTest):
             'Invalid username or password',
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
+        
+        
+    def test_form_login_invalid_credentials(self):
+        # User open login page
+        self.browser.get(
+            self.live_server_url + reverse('authors:login')
+        )
+        
+        # User sees the login form
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+        
+        # User try to send invalid user and/or password
+        username = self.get_by_placeholder(form, 'Type your username')
+        password = self.get_by_placeholder(form, 'Type your password')
+        username.send_keys('invalid_user')
+        password.send_keys('invalid_password')
+        
+        # User sends the form
+        form.submit()
+        
+        # Sees the error message
+        self.assertIn(
+            'Invalid credentials',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
     
