c5c30d07953c7f9dfd9608faaf161b3f2ad1d649
Lucas Luza <luzalucas1999@gmail.com>
2024-04-09 15:45:52 -0300

    Saving the recipe changes on the data base

--- authors/views.py
+++ authors/views.py
@@ -114,8 +114,21 @@ def dashboard_recipe_edit(request, id):
     
     form = AuthorRecipeForm(
         data=request.POST or None,
+        files=request.FILES or None,
         instance=recipe,
     )
+    
+    if form.is_valid():
+        recipe = form.save(commit=False)
+        
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+        
+        recipe.save()
+        
+        messages.success(request, 'Your recipe has been saved successfully')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
     
     return render(
         request, 'authors/pages/dashboard_recipe.html', context={
