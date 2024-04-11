58b21e9ea4da373dc6476e9f6c024fadef7a5de7
Lucas Luza <luzalucas1999@gmail.com>
2024-04-11 14:45:38 -0300

    Re-creating a base for home, category and serach with ListView

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,34 @@ from .models import Recipe
 from django.http.response import Http404
 from django.db.models import Q
 from utils.pagination import make_pagination
+from django.views.generic import ListView
 
 import os
 
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
+
+
+class RecipeListViewBase(ListView):
+    model = Recipe
+    context_object_name = 'recipes'
+    ordering = ['-id']
+    template_name = 'recipes/pages/home.html'
+    
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            is_published=True,
+        )
+        return qs
+    
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_object, pagination_range = make_pagination(
+            self.request, ctx.get('recipes'), PER_PAGE)
+        ctx.update(
+            { 'recipes': page_object, 'pagination_range': pagination_range})
+        return ctx
 
 
 def home(request):
