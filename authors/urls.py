168163cc3a023c9ba55b7c0c5d07254a8c8540d2
Lucas Luza <luzalucas1999@gmail.com>
2024-04-18 17:53:25 -0300

    Creating the html menu structure and adding CSS

--- authors/urls.py
+++ authors/urls.py
@@ -11,14 +11,20 @@ urlpatterns = [
     path('login/create/', views.login_create, name="login_create"),
     path('logout/', views.logout_view, name="logout"),
     path('dashboard/', views.dashboard, name="dashboard"),
-    path('dashboard/recipe/new/', 
+    path(
+        'dashboard/recipe/new/', 
         views.DashboardRecipe.as_view(), 
-         name="dashboard_recipe_create"),
-    path('dashboard/recipe/delete/',
+         name="dashboard_recipe_create"
+         ),
+    path(
+        'dashboard/recipe/delete/',
         views.DashboardRecipeDelete.as_view(), 
-        name='dashboard_recipe_delete'),
-    path('dashboard/recipe/<int:id>/edit/', 
+        name='dashboard_recipe_delete'
+        ),
+    path(
+        'dashboard/recipe/<int:id>/edit/', 
         views.DashboardRecipe.as_view(), 
-        name="dashboard_recipe_edit"),
+        name="dashboard_recipe_edit"
+        ),
 ]
 
