99b45daa2560cef23b97f50deb55056431ba53a3
Lucas Luza <luzalucas1999@gmail.com>
2024-04-18 15:47:55 -0300

    Organizing settings.py into a Python package

---
+++ project/settings/messages.py
@@ -0,0 +1,10 @@
+from django.contrib.messages import constants
+
+
+MESSAGE_TAGES = {
+    constants.DEBUG: 'message-debug',
+    constants.ERROR: 'message-error',
+    constants.INFO: 'message-info',
+    constants.SUCCESS: 'message-success',
+    constants.WARNING: 'message-warning',
+}
