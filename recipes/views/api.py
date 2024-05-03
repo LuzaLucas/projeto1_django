2312a2c465d81d12979d4ca0ae499bdaabdeb2b3
Lucas Luza <luzalucas1999@gmail.com>
2024-05-03 14:48:34 -0300

    Using ViewSet on DRF for GET, POST, PATCH, PUT and DELETE

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,11 +1,10 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
 from tag.models import Tag
-from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 
 from ..serializers import TagSerializer, RecipeSerializer
 from ..models import Recipe
@@ -13,79 +12,12 @@ from ..models import Recipe
 
 class RecipeAPIv2Pagination(PageNumberPagination):
     page_size = 12
-
-
-class RecipeAPIv2List(ListCreateAPIView):
+    
+    
+class RecipeAPIv2ViewSet(ModelViewSet):    
     queryset = Recipe.objects.get_published() # type: ignore
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
-    
-    # def get(self, request):
-    #     recipes = Recipe.objects.get_published()[:10] # type: ignore
-    #     serializer = RecipeSerializer(
-    #         instance=recipes,
-    #         many=True,
-    #         context={'request': request},
-    #     )
-    #     return Response(serializer.data)
-        
-    # def post(self, request):
-    #     serializer = RecipeSerializer(
-    #         data=request.data,
-    #         context={'request': request}
-    #         )
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save(
-    #         author_id=1,
-    #         category_id=1,
-    #         tags=[1, 2, 4, 5],
-    #     )
-    #     return Response(
-    #         serializer.data,
-    #         status=status.HTTP_201_CREATED
-    #     )
-
-
-class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
-    queryset = Recipe.objects.get_published() # type: ignore
-    serializer_class = RecipeSerializer
-    pagination_class = RecipeAPIv2Pagination
-    
-    # def get_recipe(self, pk):
-    #     recipe = get_object_or_404(
-    #         Recipe.objects.get_published(), # type: ignore
-    #         pk=pk
-    #     )
-    #     return recipe
-    
-    # def get(self, request, pk):
-    #     recipe = self.get_recipe(pk)
-    #     serializer = RecipeSerializer(
-    #         instance=recipe,
-    #         many=False,
-    #         context={'request': request},
-    #     )
-    #     return Response(serializer.data)
-
-    # def patch(self, request, pk):
-    #     recipe = self.get_recipe(pk)
-    #     serializer = RecipeSerializer(
-    #         instance=recipe,
-    #         data=request.data,
-    #         many=False,
-    #         context={'request': request},
-    #         partial=True,
-    #     )
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     return Response(
-    #         serializer.data,
-    #     )
-
-    # def delete(self, request, pk):
-    #     recipe = self.get_recipe(pk)
-    #     recipe.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
