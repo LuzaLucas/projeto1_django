7c729711f6c4379ef1759e6f5af054e6c60697e5
Lucas Luza <luzalucas1999@gmail.com>
2024-05-06 15:13:15 -0300

    Creating a personalized URL Me on Routers (DRF)

--- authors/views/api.py
+++ authors/views/api.py
@@ -1,7 +1,10 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from ..serializers import AuthorSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+from ..serializers import AuthorSerializer
 
 
 class AuthorViewSet(ReadOnlyModelViewSet):
@@ -12,3 +15,14 @@ class AuthorViewSet(ReadOnlyModelViewSet):
         User = get_user_model()
         qs = User.objects.filter(username=self.request.user.username) # type: ignore
         return qs
+
+    @action(
+        methods=['get'],
+        detail=False,
+    )
+    def me(self, request, *args, **kwargs):
+        obj = self.get_queryset().first()
+        serializer = self.get_serializer(
+            instance=obj,
+        )
+        return Response(serializer.data)
