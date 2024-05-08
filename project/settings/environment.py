a5b063df49a6afb74bdc4f98488cd13fe40f21af
Lucas Luza <luzalucas1999@gmail.com>
2024-05-08 16:06:48 -0300

    installing CORS and configuring it to allow one host to send request to the server

--- project/settings/environment.py
+++ project/settings/environment.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+from utils.environment import get_env_variable, parse_comma_sep_str_to_list
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -9,6 +11,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
 DEBUG = True if os.environ.get('DEBUG') == '1' else False
 
 ALLOWED_HOSTS: list[str] = []
+
+CORS_ALLOWED_ORIGINS: list[str] = parse_comma_sep_str_to_list(
+    get_env_variable('CSRF_TRUSTED_ORIGINS')
+)
 
 ROOT_URLCONF = 'project.urls'
 
